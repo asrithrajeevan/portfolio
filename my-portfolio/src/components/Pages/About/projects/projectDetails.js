95652921a5949979d18c3895936d6390ed49e042
Asrith Rajeevan <asrithrajeevan@Asriths-MacBook-Air.local>
2024-05-12 14:39:41 +0530

    feedbachchange

---
+++ my-portfolio/src/components/Pages/About/projects/projectDetails.js
@@ -0,0 +1,69 @@
+export const allProjectsDone = {
+    webApps: [
+      {
+        "img" : 'hospital.png',
+        "name": "City Hospital",
+        "link": "https://github.com/asrithrajeevan/City-hospital",
+        "skills": ["Python", "Django", "HTML", "CSS", "Bootrap", "Mysql", "Sql"],
+        'description': 'City Hospital is a Python Django web application that caters to the needs of a hospital firm, offering admin and user interfaces. '
+      },
+      {
+        "img" : 'ecommerce.jpg',
+        "name": "RentSell",
+        "link": "",
+        "skills": ["Google App Engine", "Python", "Django", "React", "Google Cloud Storage", "Cloud SQL", "Cloud Datastore", "Firebase", "Bootstrap", "HTML", "CSS", "Ajax", "Jquery"],
+        'description': 'Rentsell is an e-commerce platform where users can sell, rent, buy or service products. The application is built using Django and ReactJS. Resource files are stored in Cloud Storage. Payment to multiple users at a time is managed by PayPal Adaptive Payment API.'
+
+      },
+      {
+        "img" : 'ceba.jpg',
+        "name": "Ceba Construction",
+        "link": "https://github.com/asrithrajeevan/ceba_construction",
+        "skills": ["Python", "Django","Google Cloud Platform", "HTML", "CSS", "Bootstrap", "Google App Engine"],
+        'description': 'CEBA Construction is a Python-based web application that serves as an informative platform for introducing users to the CEBA Construction company. The primary objective of the application is to showcase the projects undertaken by CEBA Construction, outline their range of services, and provide users with contact information to facilitate communication for construction-related inquiries.'
+
+      },
+      {
+        "img" : 'netflix.png',
+        "name": "Netflix Clone",
+        "link": "https://github.com/asrithrajeevan/netflixClone",
+        "skills": ["React.js", "Firebase", "Jsx" , "CSS", "Bootstrap", "Javascript", "HTML", "Redux"],
+        'description': 'The Netflix Clone project is a React.js-based web application aimed at replicating the core functionalities and user interface of the popular streaming platform, Netflix. Unlike the original Netflix platform, which hosts a vast library of movies and TV shows, the Netflix Clone project focuses specifically on displaying trailers for movies across various genres, including action, romance, thriller, and horror.'
+
+      },
+      {
+        "img" : 'portfolio.png',
+        "name": "Portfolio",
+        "link": "https://github.com/asrithrajeevan/portfolio",
+        "skills": ["React.js", "Firebase", "Jsx" , "CSS", "Bootstrap", "Javascript", "HTML"],
+        'description': 'The Portfolio Website is a dynamic and responsive web application built using React.js, Firebase, HTML, CSS, Bootstrap, and Context APIs. It serves as a digital portfolio for showcasing my professional profile, projects, skills, contact information and achievements to potential employers, clients, or collaborators.'
+
+      },
+      {
+        "img" : 'todo.png',
+        "name": "Todo Web App",
+        "link": "",
+        "skills": ["React.js", "Jsx" , "CSS", "Bootstrap", "Javascript", "HTML"],
+        'description': 'The Todo Web App is a simple yet powerful task management application built using React.js. It provides users with the ability to create, manage, and organize their daily tasks and to-do lists in an intuitive and efficient manner.'
+
+      }
+    ],
+    mobileApps: [
+      {
+        "img" : 'inkart.jpg',
+        "name": "inkart",
+        "link": "https://github.com/asrithrajeevan/React-Native-App-InKart-/tree/develop",
+        "skills": ["React Native", "Firebase", "Redux", "Context Api", "AsyncStorage", "Rest Api", "Javascript", "Jsx"],
+        'description': "iKartify is a React Native-based e-commerce mobile app featuring robust authentication with Google sign-in, phone number verification, and guest access. The app facilitates seamless user registration and integrates Razorpay for secure payment processing.iKartify ensures accurate product delivery tracking. Key components include a user-friendly cart and wishlist, while backend data is managed efficiently through Firestore."
+        
+      },
+      {
+        "img" : 'wegloo.jpg',
+        "name": "wegloo",
+        "link": "https://github.com/asrithrajeevan/wegloo-mobile",
+        "skills": ["React Native", "Firebase", "Redux", "Context Api", "AsyncStorage", "Rest Api", "Javascript", "Jsx"],
+        "description": "Wegloo is a modern social media app developed with React Native, offering seamless user registration via Google or phone number. With Firebase backend support and Redux for state management, users enjoy a smooth experience.The platform features a dynamic feed where users can like, comment, and post content. Wegloo integrates Razorpay for secure in-app payments, making transactions hassle-free."
+
+      }
+    ]
+  }
